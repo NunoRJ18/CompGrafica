c50c7b1c4fdfd55e12c22ae6d3c14d6a7876c4b1
Gabriel <gabrielrodriguesmarquesvalim@gmail.com>
2025-09-22 12:59:54 -0300

    coloquei o fator

--- tarefa_aula08.cpp
+++ tarefa_aula08.cpp
@@ -9,21 +9,23 @@ int main(){
 
    LineStrip L{P.size()};
 
+   float fator= float(2.0/3.0);
    mat3 M1 = rotate_2d(1.57);
    mat3 M2 = scale(0.1,0.1);
+   mat3 M3 = window(-1,1,-fator,fator);
   
-   ImageRGB G(600, 500);
+   ImageRGB G(600,400);
    G.fill(black);
 
     for (double i=0; i<200; i++){
         mat3 R = rotate_2d(i*M_PI/16);
         double p = (1+((i)/10));
         mat3 T = translate(p*cos(i*M_PI/16),p*sin(i*M_PI/16));
-        render2d(M2*T*R*P, L, green, G);
+        render2d(M3*M2*T*R*P, L, green, G);
 
     }
 
     
 
-   G.save("output.png");
+   G.save("output1.png");
 }
